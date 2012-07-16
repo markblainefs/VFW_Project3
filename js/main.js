209257acc38f6d792cef2af610cc1e90d2b36256
Mark Blaine <markopollo@Marks-MacBook-Pro.local>
2012-07-16 11:01:17 -0500

    Added editItems function

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ window.addEventListener("DOMContentLoaded", function(){
 			item.group		= ["Location:", $('groups').value];
 			item.dueDate	= ["Due date:", $('dueDate').value];
 			item.importance	= ["Importance:", $('importance').value];
-			item.priority 	= ["Priority:", priorityValue];
+			item.priority 	= ["Because:", priorityValue];
 			item.honey 		= ["List for:", $('forHoney').value];
 			item.specialInstructions = ["Details:", $('specialInstructions').value];
 		//Save data to local storage: Use Stringify to convert our object to a string.
@@ -81,6 +81,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		alert("Honey Do Saved!");
 	}
 	
+	//Display list
 	function getData(){
 		toggleControls("on");
 		if (localStorage.length===0){
@@ -95,6 +96,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		$('items').style.display = "block";
 		for (var i=0, len=localStorage.length; i<len; i++){
 			var makeli = document.createElement('li');
+			var linksLi = document.createElement('li');
 			makeList.appendChild(makeli);
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
@@ -107,10 +109,63 @@ window.addEventListener("DOMContentLoaded", function(){
 				makeSubList.appendChild(makeSubli);
 				var optSubText = obj[n][0]+" "+obj[n][1];
 				makeSubli.innerHTML = optSubText;
-				
+				makeSubList.appendChild(linksLi);
 			}
+			makeItemLinks(localStorage.key(i), linksLi); //Create our edit and delet buttons for each item in local storage
 		}
 	}
+	
+	//Make item links
+	//Will create edit and delete links for each stored item when displayed
+	function makeItemLinks (key, linksLi){
+		//add edit single item link
+		var editLink = document.createElement('a');
+		editLink.href = "#";
+		editLink.key = key;
+		var editText = "Edit Task";
+		editLink.addEventListener("click", editItem);
+		editLink.innerHTML = editText;
+		linksLi.appendChild(editLink);
+		
+		//add delete single item link
+		var deleteLink = document.createElement('a');
+		deleteLink.href = "#";
+		deleteLink.key = key;
+		var deleteText = "Delete Task";
+		//deleteLink.addEventListener("click", deleteItem);
+		deleteLink.innerHTML = deleteText;
+		linksLi.appendChild(deleteLink);
+	}
+	
+	function editItem(){
+		//Grab the item from the Local Storage
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+		
+		//Show the form and hide the list
+		toggleControls("off");
+		
+		//populate the form fields with the current localStorage values
+		$('taskName').value = item.taskName[1];
+		$('groups').value = item.group[1];
+		$('dueDate').value = item.dueDate[1];
+		$('importance').value = item.importance[1];
+		//Set the radio button
+		var radios = document.forms[0].priority;
+		for (var i=0; i<radios.length; i++){
+			if (radios[i].value == "I'm awesome" && item.priority[1] == "I'm awesome"){
+				radios[i].setAttribute("checked", "checked");
+			} else if (radios[i].value == "I need to" && item.priority[1] == "I need to"){
+				radios[i].setAttribute("checked", "checked");
+			} else if (radios[i].value == "I'm in trouble" && item.priority[1] == "I'm in trouble"){
+				radios[i].setAttribute("checked", "checked");
+			}
+		}
+		$('specialInstructions').value = item.specialInstructions[1];
+	
+	}
+	
+	
 	
 	//Function to clear the local storage
 	function clearLocal(){
